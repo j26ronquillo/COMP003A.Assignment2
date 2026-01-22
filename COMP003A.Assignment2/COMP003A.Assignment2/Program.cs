40727166278a5611f292d36b6a7a0eef46a352a1
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-01-21 22:06:33 -0800

    made the inputs and the concatenation for the name

---
+++ COMP003A.Assignment2/COMP003A.Assignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first name: ");
+            string name1 = Console.ReadLine();
+            Console.Write("Enter second name: ");
+            string name2 = Console.ReadLine();
+            Console.Write("Enter number 1: ");
+            string value1 = Console.ReadLine();
+            Console.Write("Enter number 2: ");
+            string value2 = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Name Formats\n------------");
+            Console.Write("Using concatenation: ");
+            Console.Write(name1 + ", " + name2);
+
+            
+        }
+    }
+}
